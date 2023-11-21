4e3fbb280c3830be5d8226028b1f8df5bc1ddf0d
kavandalal <kavandalal003@gmail.com>
2023-11-21 23:46:12 +0530

    type bug fix

--- components/email-template/types.tsx
+++ components/email-template/types.tsx
@@ -1,17 +1,21 @@
+type BodyItem = Array<{
+	id: string;
+	cells: number[];
+	columns: Array<{
+		id: string;
+		contents: Array<{}>;
+		values: {};
+	}>;
+	values: {};
+}>;
+
 type JSONTemplate = {
 	counters: Record<string, number>;
 	body: {
 		id: string | undefined;
-		rows: Array<{
-			id: string;
-			cells: number[];
-			columns: Array<{
-				id: string;
-				contents: Array<{}>;
-				values: {};
-			}>;
-			values: {};
-		}>;
+		rows: BodyItem;
+		headers: BodyItem;
+		footers: BodyItem;
 		values: {};
 	};
 	schemaVersion?: number | undefined;
